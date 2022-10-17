362d07bf64ada2ab1c778cd6ac0c7a2105c7c138
DaWoblefet <leonardcraft64@gmail.com>
2022-10-17 15:12:31 -0500

    Add another excellent joke

--- commands/fun.js
+++ commands/fun.js
@@ -131,7 +131,8 @@ exports.commands = {
 			"My friend couldn't afford to pay her water bill, so I sent her a \"get well soon\" card.",
 			"What's the difference between in-laws and outlaws? Outlaws are wanted!",
 			"I went to the zoo yesterday and saw a bagel in a cage. The zookeeper informed me it was bread in captivity!",
-			"Why did the robber have to retire? He just couldn't take it anymore!"
+			"Why did the robber have to retire? He just couldn't take it anymore!",
+			"I used to think I was indecisive, but now, I'm not so sure."
 		];
 
 		let jokeNum = arg === "latest" ? jokeList.length - 1 : parseInt(arg);
