91eebb6c8496a490207f5949e57eace08dce045a
Leonard Craft III <leonardcraft64@gmail.com>
2024-04-22 20:18:54 -0500

    Update default format

--- commands/general.js
+++ commands/general.js
@@ -53,7 +53,7 @@ exports.commands = {
 		let tourname;
 		let tourObject;
 		let isOfficial = false;
-		const defaultTour = "regf";
+		const defaultTour = "regg";
 		const defaultTourType = 'elimination';
 		const defaultTourPlayerCap = 128;
 		let isDoubleElimination = false;
